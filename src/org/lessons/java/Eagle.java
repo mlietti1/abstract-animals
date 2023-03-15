2b61e6a3fbe179b489c5830d2d69d6da69ccf5b7
mlietti1 <mcf.lietti@outlook.com>
2023-03-15 15:39:42 +0100

    final commit

---
+++ src/org/lessons/java/Eagle.java
@@ -0,0 +1,36 @@
+package org.lessons.java;
+
+public class Eagle extends Animal implements Flier{
+
+    private final String sound;
+
+    private final String food;
+
+    public Eagle(String name) {
+        super(name);
+        this.sound = "Screech";
+        this.food = "meat";
+    }
+
+    @Override
+    public String toString() {
+        return "Eagle{" + super.toString() + '\'' +
+                "sound='" + sound + '\'' +
+                ", food='" + food + '\'' +
+                '}';
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(sound);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("I'm eating "+ food);    }
+
+    @Override
+    public void fly() {
+        System.out.println("My name is " + getName() + ", " + sound + "!" + " I'm flying!");
+    }
+}
---
+++ src/org/lessons/java/Flier.java
@@ -0,0 +1,5 @@
+package org.lessons.java;
+
+public interface Flier {
+    void fly();
+}
